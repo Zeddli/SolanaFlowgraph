2c6e1b5f9ea9726b2c7caffbd30b4722420c6706
Zhijian <ruj8320@gmail.com>
2025-04-24 11:42:03 +0800

    header

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 import Link from 'next/link';
 import { useState } from 'react';
-import Header from '@/app/components/Header';
+import Header from './components/Header';
 
 export default function Home() {
   return (
