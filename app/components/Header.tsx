835a8e39a6784de4a998ba7fe49a9b8bdf7e14c0
Zhi Jian <157445297+Zeddli@users.noreply.github.com>
2025-04-25 10:16:57 +0800

    Update Header.tsx

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -130,7 +130,7 @@ export default function Header() {
             ))}
             
             <a
-              href="https://github.com/Zeddli/solana-flowgraph"
+              href="https://github.com/Zeddli/SolanaFlowgraph"
               target="_blank"
               rel="noopener noreferrer"
               className="block pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700"
